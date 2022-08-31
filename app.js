0d06c7e41f1a0ce3a8105805c53d9307015bffc0
yongxialee leelianou <dejnaj@hotmail.com>
2022-08-31 13:04:48 -0600

    add files

---
+++ app.js
@@ -0,0 +1,99 @@
+//*********************************/
+// Destructuring Exercise
+//*********************************/
+
+// Object Destructuring 1
+// what does the following code return/print?
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
+
+//Object Destructuring 2
+//whta does the following code return?
+let planetFacts = {
+    numberPlanets: 8,
+    yearNeptuneDiscovered: 1846,
+    yearMarsDiscovered: 1659
+  };
+  
+  let {numberPlanets, ...discoveryYears} = planetFacts;
+  
+  console.log(discoveryYears); // {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+  //Object Destructuring 3
+  //what does the following code return?
+  function getUserData({firstName, favoriteColor="green"}){
+    return `Your name is ${firstName} and you like ${favoriteColor}`;
+  }
+  
+  getUserData({firstName: "Alejandro", favoriteColor: "purple"}) //'Your name is Alejandro and you like purple'
+  getUserData({firstName: "Melissa"}) // 'Your name is Melissa and you like green'
+  getUserData({}) // 'Your name is undefined and you like green
+
+  //Array Destructuring 1
+  //what does the following code return?
+
+let [first,second,third] =['Maya','Marisa','Chi'];
+console.log(first); //'Maya
+console.log(second); //'Marisa'
+console.log(third); //'Chi'
+
+
+//Array Destructuring 2
+//what does the following code return?
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+  ]
+  
+  console.log(raindrops); // 'Raindrops on the roses'
+  console.log(whiskers); // 'whiskers on kittens'
+  console.log(aFewOfMyFavoriteThings); // "Bright copper kettles","warm woolen mittens","Brown paper packages tied up with strings"
+
+
+  //Array Destructuring 3
+  //what does the following code return?
+  let numbers= [10,20,30];
+  [numbers[1],numbers[2]]=[numbers[2],numbers[1]];
+  console.log(numbers); //[10,30,20]
+
+  //ES2015 refactoring
+  /*
+var obj = {
+  numbers: {
+    a: 1,
+    b: 2
+  }
+};
+
+var a = obj.numbers.a;
+var b = obj.numbers.b;
+  */
+
+//ES2015 Object Destructuring
+const obj={
+    numbers : {
+        a : 1,
+        b : 2
+    }
+};
+
+const {a,b} =obj.numbers;
+
+//RaceResult()
+//write a function called raceResults which accepts a single array argument; it should return an object wirh the key first,second, third, and rest.
+//first : the first elemetn in the array
+//second: the second element in the array
+//third: the third element in the array
+//rest: all other elements in the array
+
+const raceResults =([first,second,third,...rest]) =>{
+    return ({
+        first,second,third,rest})
+};
+raceResults(['Tom','Margaret','Allison','David','Pierre']);
